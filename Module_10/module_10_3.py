7dcc5c86800b2faa7343e5c7c0b1b02d5ba0bbd3
Krochi <77779207+Krochi@users.noreply.github.com>
2024-09-27 13:21:59 +0300

    Update module_10_3.py

--- Module_10/module_10_3.py
+++ Module_10/module_10_3.py
@@ -28,56 +28,89 @@
 
 
 #Решение:
-import threading
-import random
+# Задача "Банковские операции":
+# Необходимо создать класс Bank со следующими свойствами:
+#
+# Атрибуты объекта:
+# balance - баланс банка (int)
+# lock - объект класса Lock для блокировки потоков.
+#
+# Методы объекта:
+# Метод deposit:
+# Будет совершать 100 транзакций пополнения средств.
+# Пополнение - это увеличение баланса на случайное целое число от 50 до 500.
+# Если баланс больше или равен 500 и замок lock заблокирован - lock.locked(), то разблокировать его методом release.
+# После увеличения баланса должна выводится строка "Пополнение: <случайное число>. Баланс: <текущий баланс>".
+# Также после всех операций поставьте ожидание в 0.001 секунды, тем самым имитируя скорость выполнения пополнения.
+# Метод take:
+# Будет совершать 100 транзакций снятия.
+# Снятие - это уменьшение баланса на случайное целое число от 50 до 500.
+# В начале должно выводится сообщение "Запрос на <случайное число>".
+# Далее производится проверка: если случайное число меньше или равно текущему балансу, то произвести снятие,
+# уменьшив balance на соответствующее число и вывести на экран "Снятие: <случайное число>. Баланс: <текущий баланс>".
+# Если случайное число оказалось больше баланса, то вывести строку "Запрос отклонён, недостаточно средств"
+# и заблокировать поток методом acquiere.
+# Далее создайте объект класса Bank и создайте 2 потока для его методов deposit и take. Запустите эти потоки.
+# После конца работы потоков выведите строку: "Итоговый баланс: <баланс объекта Bank>".
+#
+# По итогу вы получите скрипт разблокирующий поток до баланса равному 500 и больше или блокирующий,
+# когда происходит попытка снятия при недостаточном балансе.
+
+
+#Решение:
 import time
+import random
+import threading
 
 class Bank:
     def __init__(self):
         self.balance = 0
         self.lock = threading.Lock()
+        self.balance_lock = threading.Lock()
+        self.deposits_done = False
 
     def deposit(self):
         for _ in range(100):
-            amount = random.randint(50,500)
+            amount = random.randint(50, 500)
+            with self.lock:
+                self.balance += amount
+                print(f"Пополнение: {amount}. Баланс: {self.balance}")
+                if self.balance_lock.locked() and self.balance >= 500:
+                    self.balance_lock.release()
+                    time.sleep(0.001)
         with self.lock:
-            self.balance += amount
-            print(f"Пополнение: {amount}, Баланс:{self.balance}")
-            time.sleep(0.001)
+            self.deposits_done = True
+            if self.balance_lock.locked():
+                self.balance_lock.release()
 
-        def take (self):
-        failed = 0
+
+    def take(self):
         for _ in range(100):
-            amount = random.randint(50,500)
-            print(f"Запрос на {amount}")
+            amount = random.randint(50, 500)
+            print(f"Запрос на: {amount}")
             with self.lock:
                 if amount <= self.balance:
                     self.balance -= amount
-                    print(f"Снятие: {amount} Баланас {self.balance}")
-                    failed =0
+                    print(f"Снятие: {amount}. Баланс: {self.balance}")
                 else:
-                    print("Запрос отклонён, недостаточно средств")
-                    failed +=1
-                    if failed >= 3:
+                    print(f"Запрос отклонён, недостаточно средств.")
+                    if not self.deposits_done:
+                        self.lock.release()
+                        self.balance_lock.acquire()
+                    else:
                         break
         time.sleep(0.001)
 
 
-# Создание объекта банка
 bk = Bank()
 
-# Создание потоков
+# Т.к. методы принимают self, в потоки нужно передать сам объект класса Bank
 th1 = threading.Thread(target=Bank.deposit, args=(bk,))
 th2 = threading.Thread(target=Bank.take, args=(bk,))
 
-# # Запуск
 th1.start()
 th2.start()
-
-# # Ожидание
 th1.join()
 th2.join()
 
-
-# Вывод итогового баланса
 print(f'Итоговый баланс: {bk.balance}')
